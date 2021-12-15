c7b7a204a960ece4b9e2a6aad06d69aebd69977d
Arnaud Saulou <a.saulou@gmail.com>
2021-12-15 11:57:33 +0100

    test parseUserInput assert ADD_TASK, REMOVE_TASK, MARK_AS_DONE, MARK_AS_TODO, EXIT on dynamic userInput

--- src/org/kata_taskmanager/Parser.java
+++ src/org/kata_taskmanager/Parser.java
@@ -4,11 +4,13 @@ public class Parser {
 
     public static Action parseUserInput(String userInput) throws InvalidUserInputException{
 
-        return switch (userInput) {
-            case "+ description" -> Action.ADD_TASK;
-            case "- 1" -> Action.REMOVE_TASK;
-            case "x 1" -> Action.MARK_AS_DONE;
-            case "o 1" -> Action.MARK_AS_TODO;
+        String[] userInputSplited = userInput.split(" ");
+
+        return switch (userInputSplited[0]) {
+            case "+" -> Action.ADD_TASK;
+            case "-" -> Action.REMOVE_TASK;
+            case "x" -> Action.MARK_AS_DONE;
+            case "o" -> Action.MARK_AS_TODO;
             case "q" -> Action.EXIT;
             default -> throw new InvalidUserInputException(userInput);
         };
